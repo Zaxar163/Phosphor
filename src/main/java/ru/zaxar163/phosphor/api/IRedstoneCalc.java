c6d9993e5bfa081a4d38101a3c78cee769b5d6af
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-06-25 12:59:42 +0300

    Critical start up fix.

--- src/main/java/ru/zaxar163/phosphor/api/IRedstoneCalc.java
+++
@@ -1,9 +0,0 @@
-package ru.zaxar163.phosphor.api;
-
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-public interface IRedstoneCalc {
-	IBlockState calculateCurrentChanges0(World worldIn, BlockPos pos1, BlockPos pos2, IBlockState state);
-}
