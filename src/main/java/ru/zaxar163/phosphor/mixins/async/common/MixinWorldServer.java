fe28b4a72d745d8be8a97ff36a2a46320c07edd2
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-06-25 21:37:27 +0300

    Async fixes.

--- src/main/java/ru/zaxar163/phosphor/mixins/async/common/MixinWorldServer.java
+++ src/main/java/ru/zaxar163/phosphor/mixins/async/common/MixinWorldServer.java
@@ -1,6 +1,7 @@
 package ru.zaxar163.phosphor.mixins.async.common;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,16 +14,22 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.CrashReportCategory;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 import net.minecraft.util.ReportedException;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.NextTickListEntry;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.storage.ISaveHandler;
 import net.minecraft.world.storage.WorldInfo;
 import ru.zaxar163.phosphor.api.AsyncTick;
@@ -110,6 +117,245 @@ public abstract class MixinWorldServer extends World {
 
             this.profiler.endSection();
         }
+    }
+
+	@Override
+    public void updateEntities()
+    {
+        this.profiler.startSection("entities");
+        this.profiler.startSection("global");
+
+        for (int i = 0; i < this.weatherEffects.size(); ++i)
+        {
+            Entity entity = this.weatherEffects.get(i);
+
+            try
+            {
+                if(entity.updateBlocked) continue;
+                ++entity.ticksExisted;
+                entity.onUpdate();
+            }
+            catch (Throwable throwable2)
+            {
+                CrashReport crashreport = CrashReport.makeCrashReport(throwable2, "Ticking entity");
+                CrashReportCategory crashreportcategory = crashreport.makeCategory("Entity being ticked");
+
+                if (entity != null)
+                {
+                    entity.addEntityCrashInfo(crashreportcategory);
+                }
+                
+                if (net.minecraftforge.common.ForgeModContainer.removeErroringEntities)
+                {
+                    net.minecraftforge.fml.common.FMLLog.log.fatal("{}", crashreport.getCompleteReport());
+                    removeEntity(entity);
+                }
+                else
+                throw new ReportedException(crashreport);
+            }
+
+            if (entity.isDead)
+            {
+                this.weatherEffects.remove(i--);
+            }
+        }
+
+        this.profiler.endStartSection("remove");
+        this.loadedEntityList.removeAll(this.unloadedEntityList);
+
+        for (int k = 0; k < this.unloadedEntityList.size(); ++k)
+        {
+            Entity entity1 = this.unloadedEntityList.get(k);
+            int j = entity1.chunkCoordX;
+            int k1 = entity1.chunkCoordZ;
+
+            if (entity1.addedToChunk && this.isChunkLoaded(j, k1, true))
+            {
+                this.getChunk(j, k1).removeEntity(entity1);
+            }
+        }
+
+        for (int l = 0; l < this.unloadedEntityList.size(); ++l)
+        {
+            this.onEntityRemoved(this.unloadedEntityList.get(l));
+        }
+
+        this.unloadedEntityList.clear();
+        this.tickPlayers();
+        this.profiler.endStartSection("regular");
+
+        for (int i1 = 0; i1 < this.loadedEntityList.size(); ++i1)
+        {
+            Entity entity2 = this.loadedEntityList.get(i1);
+            Entity entity3 = entity2.getRidingEntity();
+
+            if (entity3 != null)
+            {
+                if (!entity3.isDead && entity3.isPassenger(entity2))
+                {
+                    continue;
+                }
+
+                entity2.dismountRidingEntity();
+            }
+
+            this.profiler.startSection("tick");
+
+            if (!entity2.isDead && !(entity2 instanceof EntityPlayerMP))
+            {
+            	Runnable r = () -> {
+                try
+                {
+                    net.minecraftforge.server.timings.TimeTracker.ENTITY_UPDATE.trackStart(entity2);
+                    this.updateEntity(entity2);
+                    net.minecraftforge.server.timings.TimeTracker.ENTITY_UPDATE.trackEnd(entity2);
+                }
+                catch (Throwable throwable1)
+                {
+                    CrashReport crashreport1 = CrashReport.makeCrashReport(throwable1, "Ticking entity");
+                    CrashReportCategory crashreportcategory1 = crashreport1.makeCategory("Entity being ticked");
+                    entity2.addEntityCrashInfo(crashreportcategory1);
+                    if (net.minecraftforge.common.ForgeModContainer.removeErroringEntities)
+                    {
+                        net.minecraftforge.fml.common.FMLLog.log.fatal("{}", crashreport1.getCompleteReport());
+                        removeEntity(entity2);
+                    }
+                    else
+                    throw new ReportedException(crashreport1);
+                }
+            	};
+            	if (OptimEnginePlugin.CFG.asyncEntity) AsyncTick.INSTANCE.forceRunS(r);
+            	else r.run();
+            }
+
+            this.profiler.endSection();
+            this.profiler.startSection("remove");
+
+            if (entity2.isDead)
+            {
+                int l1 = entity2.chunkCoordX;
+                int i2 = entity2.chunkCoordZ;
+
+                if (entity2.addedToChunk && this.isChunkLoaded(l1, i2, true))
+                {
+                    this.getChunk(l1, i2).removeEntity(entity2);
+                }
+
+                this.loadedEntityList.remove(i1--);
+                this.onEntityRemoved(entity2);
+            }
+
+            this.profiler.endSection();
+        }
+
+        this.profiler.endStartSection("blockEntities");
+
+        this.processingLoadedTiles = true; //FML Move above remove to prevent CMEs
+
+        if (!this.tileEntitiesToBeRemoved.isEmpty())
+        {
+            for (Object tile : tileEntitiesToBeRemoved)
+            {
+            	((TileEntity)tile).onChunkUnload();
+            }
+
+            // forge: faster "contains" makes this removal much more efficient
+            java.util.Set<TileEntity> remove = java.util.Collections.newSetFromMap(new java.util.IdentityHashMap<>());
+            remove.addAll(tileEntitiesToBeRemoved);
+            this.tickableTileEntities.removeAll(remove);
+            this.loadedTileEntityList.removeAll(remove);
+            this.tileEntitiesToBeRemoved.clear();
+        }
+
+        Iterator<TileEntity> iterator = this.tickableTileEntities.iterator();
+
+        while (iterator.hasNext())
+        {
+            TileEntity tileentity = iterator.next();
+            if (!tileentity.isInvalid() && tileentity.hasWorld())
+            {
+                BlockPos blockpos = tileentity.getPos();
+
+                if (this.isBlockLoaded(blockpos, false) && this.worldBorder.contains(blockpos)) //Forge: Fix TE's getting an extra tick on the client side....
+                {
+                	Runnable r = () -> {
+                    try
+                    {
+                        this.profiler.func_194340_a(() ->
+                        {
+                            return String.valueOf((Object)TileEntity.getKey(tileentity.getClass()));
+                        });
+                        net.minecraftforge.server.timings.TimeTracker.TILE_ENTITY_UPDATE.trackStart(tileentity);
+                        ((ITickable)tileentity).update();
+                        net.minecraftforge.server.timings.TimeTracker.TILE_ENTITY_UPDATE.trackEnd(tileentity);
+                        this.profiler.endSection();
+                    }
+                    catch (Throwable throwable)
+                    {
+                        CrashReport crashreport2 = CrashReport.makeCrashReport(throwable, "Ticking block entity");
+                        CrashReportCategory crashreportcategory2 = crashreport2.makeCategory("Block entity being ticked");
+                        tileentity.addInfoToCrashReport(crashreportcategory2);
+                        if (net.minecraftforge.common.ForgeModContainer.removeErroringTileEntities)
+                        {
+                            net.minecraftforge.fml.common.FMLLog.log.fatal("{}", crashreport2.getCompleteReport());
+                            tileentity.invalidate();
+                            this.removeTileEntity(tileentity.getPos());
+                        }
+                        else
+                        throw new ReportedException(crashreport2);
+                    }
+                	};
+                	if (OptimEnginePlugin.CFG.asyncTiles) AsyncTick.INSTANCE.forceRunS(r);
+                	else r.run();
+                }
+            }
+
+            if (tileentity.isInvalid())
+            {
+                iterator.remove();
+                this.loadedTileEntityList.remove(tileentity);
+
+                if (this.isBlockLoaded(tileentity.getPos()))
+                {
+                    //Forge: Bugfix: If we set the tile entity it immediately sets it in the chunk, so we could be desyned
+                    Chunk chunk = this.getChunk(tileentity.getPos());
+                    if (chunk.getTileEntity(tileentity.getPos(), net.minecraft.world.chunk.Chunk.EnumCreateEntityType.CHECK) == tileentity)
+                        chunk.removeTileEntity(tileentity.getPos());
+                }
+            }
+        }
+
+        this.processingLoadedTiles = false;
+        this.profiler.endStartSection("pendingBlockEntities");
+
+        if (!this.addedTileEntityList.isEmpty())
+        {
+            for (int j1 = 0; j1 < this.addedTileEntityList.size(); ++j1)
+            {
+                TileEntity tileentity1 = this.addedTileEntityList.get(j1);
+
+                if (!tileentity1.isInvalid())
+                {
+                    if (!this.loadedTileEntityList.contains(tileentity1))
+                    {
+                        this.addTileEntity(tileentity1);
+                    }
+
+                    if (this.isBlockLoaded(tileentity1.getPos()))
+                    {
+                        Chunk chunk = this.getChunk(tileentity1.getPos());
+                        IBlockState iblockstate = chunk.getBlockState(tileentity1.getPos());
+                        chunk.addTileEntity(tileentity1.getPos(), tileentity1);
+                        this.notifyBlockUpdate(tileentity1.getPos(), iblockstate, iblockstate, 3);
+                    }
+                }
+            }
+
+            this.addedTileEntityList.clear();
+        }
+
+        this.profiler.endSection();
+        this.profiler.endSection();
     }
 
 }
