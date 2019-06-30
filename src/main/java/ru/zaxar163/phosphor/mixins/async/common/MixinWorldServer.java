22bd1ca9b9df12af78cf2d33cc296ff1a6bf2284
Zaxar163 <zahar.vcherachny@yandex.ru>
2019-06-30 09:16:16 +0300

    One line most problems.

--- src/main/java/ru/zaxar163/phosphor/mixins/async/common/MixinWorldServer.java
+++ src/main/java/ru/zaxar163/phosphor/mixins/async/common/MixinWorldServer.java
@@ -7,18 +7,13 @@ import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.crash.CrashReport;
-import net.minecraft.crash.CrashReportCategory;
-import net.minecraft.entity.Entity;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ReportedException;
 import net.minecraft.world.NextTickListEntry;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
